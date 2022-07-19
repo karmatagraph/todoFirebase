69fdb440d68c927d28b61c295209303d3a0cc5d3
karma <karmatashi954@gmail.com>
2022-07-19 16:05:15 +0545

    Initial Commit

---
+++ todoFirebase/todoFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoFirebaseApp.swift
+//  todoFirebase
+//
+//  Created by karma on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct todoFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
